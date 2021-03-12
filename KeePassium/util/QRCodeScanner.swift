c2401e5ebf547fdbc93606b538a41a46b6fe7b13
Igor Kulman <igor@kulman.sk>
2021-03-12 12:29:13 +0100

    Adding a simple QR code scanner based on YubiKit

---
+++ KeePassium/util/QRCodeScanner.swift
@@ -0,0 +1,35 @@
+//
+//  QRCodeScanner.swift
+//  KeePassium
+//
+//  Created by Igor Kulman on 12.03.2021.
+//  Copyright © 2021 Andrei Popleteev. All rights reserved.
+//
+
+import Foundation
+
+protocol QRCodeScanner: AnyObject {
+    var deviceSupportsQRScanning: Bool { get }
+
+    func scanQrCode(presenter: UIViewController, completion: @escaping (Result<String, Error>) -> Void)
+}
+
+final class YubiKitQRCodeScanner: QRCodeScanner {
+    var deviceSupportsQRScanning: Bool {
+        return YubiKitDeviceCapabilities.supportsQRCodeScanning
+    }
+
+    private let session = YKFQRReaderSession()
+
+    func scanQrCode(presenter: UIViewController, completion: @escaping (Result<String, Error>) -> Void) {
+        session.scanQrCode(withPresenter: presenter) { (data, error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+
+            if let data = data {
+                completion(.success(data))
+            }
+        }
+    }
+}
