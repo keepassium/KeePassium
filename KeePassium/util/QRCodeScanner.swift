0aa9d50508aff1293f4011de39ecad54b425e5d5
Igor Kulman <igor@kulman.sk>
2021-03-14 10:29:18 +0100

    Update KeePassium/util/QRCodeScanner.swift
    Co-authored-by: KeePassium <info@keepassium.com>

--- KeePassium/util/QRCodeScanner.swift
+++ KeePassium/util/QRCodeScanner.swift
@@ -1,10 +1,12 @@
-//
-//  QRCodeScanner.swift
-//  KeePassium
+//  KeePassium Password Manager
+//  Copyright © 2021 Andrei Popleteev <info@keepassium.com>
+// 
+//  This program is free software: you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License version 3 as published
+//  by the Free Software Foundation: https://www.gnu.org/licenses/).
+//  For commercial licensing, please contact the author.
 //
 //  Created by Igor Kulman on 12.03.2021.
-//  Copyright © 2021 Andrei Popleteev. All rights reserved.
-//
 
 import Foundation
 import KeePassiumLib
