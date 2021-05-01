0df522db41deb1be35efe7a732f5af0b96135ea1
KeePassium <info@keepassium.com>
2021-05-01 22:57:17 +0200

    Refactor: add Entry Viewer Coordinator stub

---
+++ KeePassium/database/entry/EntryViewerCoordinator.swift
@@ -0,0 +1,45 @@
+//  KeePassium Password Manager
+//  Copyright © 2021 Andrei Popleteev <info@keepassium.com>
+//
+//  This program is free software: you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License version 3 as published
+//  by the Free Software Foundation: https://www.gnu.org/licenses/).
+//  For commercial licensing, please contact the author.
+
+import KeePassiumLib
+
+protocol EntryViewerCoordinatorDelegate: AnyObject {
+    
+}
+
+final class EntryViewerCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    
+    var dismissHandler: CoordinatorDismissHandler?
+    private let router: NavigationRouter
+    private var entry: Entry
+    
+    private fieldViewerVC: ViewEntryFieldsVC
+    
+    init(router: NavigationRouter) {
+        self.router = router
+        
+        fieldViewerVC = ViewEntryFieldsVC.instantiateFromStoryboard()
+        fieldViewerVC.delegate = self
+    }
+    
+    deinit {
+        assert(childCoordinators.isEmpty)
+        removeAllChildCoordinators()
+    }
+    
+    func start() {
+        <#code#>
+    }
+    
+    public func setEntry(_ entry: Entry) {
+        self.entry = entry
+        
+        refresh()
+    }
+}
