d02b2a9a58099d146d56ea571b83becea4782d7b
KeePassium <info@keepassium.com>
2023-12-05 16:57:36 +0100

    Chore: update copyright notice

--- KeePassiumLib/KeePassiumLib/util/PasswordGeneratorParams.swift
+++ KeePassiumLib/KeePassiumLib/util/PasswordGeneratorParams.swift
@@ -1,5 +1,5 @@
 //  KeePassium Password Manager
-//  Copyright © 2018-2022 Andrei Popleteev <info@keepassium.com>
+//  Copyright © 2018–2024 KeePassium Labs <info@keepassium.com>
 //
 //  This program is free software: you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License version 3 as published
